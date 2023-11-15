e0fd770dd2d9d28a7406ae84296b07eebfd7785e
Rahuman-S <issamrahman96@gmail.com>
2023-11-15 14:12:13 +0000

    first

---
+++ src/AccountNumber.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class AccountNumber 
+{
+    private int AccountNumber;
+    private double Balance;
+    private int Pin;
+    private List<String>transactionHistory;
+    public AccountNumber() 
+    {
+        super();
+    }
+    public AccountNumber(int AccountNumber,int Pin,double initialBalance)
+    {
+        this.AccountNumber=AccountNumber;
+        this.Pin=Pin;
+        this.Balance=initialBalance;
+        this.transactionHistory=new ArrayList<>();
+
+    }
+    
+    
+}
---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) 
+     {
+        AccountNumber c1=new AccountNumber(12345,1122 , 1000);
+        Scanner sc=new Scanner(System.in);
+        int c1=sc.nextInt();
+    }
+}
